65d016d58aebe374aa0a6f30d37d6a00d45a4fac
Sumnima <sumnimarana1@gmail.com>
2018-10-20 14:15:58 -0500

    So added the new views and button function in the app

---
+++ UserSignin/ItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemViewController.swift
+//  UserSignin
+//
+//  Created by Rana,Sumnima on 10/20/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ segue:UIStoryboardSegue){}
+    @IBAction func done(_ segue:UIStoryboardSegue){}    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
