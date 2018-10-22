b52d54604506302e7ea24e0b2202596891795884
student <student@admins-macbook-air-2.local>
2018-10-22 00:30:39 -0500

    Added new functions to the food delivery.

---
+++ UserSignin/RestaurantViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantViewController.swift
+//  UserSignin
+//
+//  Created by student on 10/22/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func donecheckout(_ segue:UIStoryboardSegue){}
+    @IBAction func cancelcheckout(_ segue:UIStoryboardSegue){}
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
